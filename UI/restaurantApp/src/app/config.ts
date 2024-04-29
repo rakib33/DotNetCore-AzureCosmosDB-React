8f6d9e8937167cbd5130586c2bbc908a07936d60
Rakibul Islam <rakib33mbstu@gmail.com>
2024-04-29 09:08:23 +0600

    Please enter the commit message for your changes. Lines starting

--- UI/restaurantApp/src/app/config.ts
+++
@@ -1,2 +0,0 @@
-// config.ts
-export const API_BASE_URL = 'https://localhost:7222/';
--- UI/restaurantApp/src/app/models/restaurant.interface.ts
+++
@@ -1,8 +0,0 @@
-// restaurant.interface.ts
-export interface Restaurant {
-    id: number;
-    name: string;
-    operatingTime: string;
-    // Add other properties as needed
-  }
-  
